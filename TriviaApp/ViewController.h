ffdd2edc01d6b9e50dae0aaef1bf56be1ffe48f6
msmi1023 <msmi1023@ford.com>
2016-05-21 21:02:04 -0400

    MVP of Animal Trivia app

--- TriviaApp/ViewController.h
+++ TriviaApp/ViewController.h
@@ -8,7 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+	IBOutlet UIButton *button;
+}
+
+-(IBAction)correctAnswerPressed;
+-(IBAction)wrongAnswerPressed;
 
 
 @end
--- TriviaApp/ViewController.m
+++ TriviaApp/ViewController.m
@@ -14,14 +14,35 @@
 
 @implementation ViewController
 
+
+
+- (void)goToNextView {
+	[self performSegueWithIdentifier:@"segueToNextView" sender:self];
+}
+
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+	//[self performSelector:@selector(goToNextView) withObject:nil afterDelay:2];
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
+
+- (IBAction)correctAnswerPressed {
+	self.view.backgroundColor = [UIColor greenColor];
+	//[self performSelector:@selector(goToNextView) withObject:nil afterDelay:2];
+}
+
+- (IBAction)wrongAnswerPressed {
+	self.view.backgroundColor = [UIColor redColor];
+	[self performSelector:@selector(resetBackground) withObject:nil afterDelay:1];
+}
+
+- (void)resetBackground {
+	self.view.backgroundColor = [UIColor whiteColor];
+};
 
 @end
